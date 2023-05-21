387561252b1a1a5ba24f65ee8b032e6ded932a15
ramir <ramirezh@uabc.edu.mx>
2023-05-21 11:16:50 -0700

    Add two new columns with the names of the applications.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide', page_icon='random')
 
@@ -14,10 +15,22 @@ with col2:  # Descripción
     """
     st.info(content)
 
+# Se coloca un pequeño texto descriptivo debajo de las dos columnas superiores
 info_text = '''
 Below you can check all the applications I have made with Python. 
 Please feel free to contact me for any question or issue.
 '''
 st.info(info_text)
+
+df = pandas.read_csv('data.csv', sep=';')
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, rows in df[:10].iterrows():
+        st.header(rows['title'])
+
+with col4:
+    for index, rows in df[10:].iterrows():
+        st.header(rows['title'])
 
 
